b27f9593c45f0a104e2875aba662d620e907736c
Magb23 <m.a.guerrero1989@gmail.com>
2017-12-07 13:06:16 +0100

    Cambios 07/12

--- primos.js
+++ primos.js
@@ -1,11 +1,11 @@
 function primos (num){
-
+    
     var primo = true
     var i=2
-
+    
     while(i<num){
         if(num % i ==0){
-        return false;
+            return false;
         }
         i++;
     }
